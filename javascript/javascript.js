aadc009db3b688723ae9c6f12568d0c0118efc1c
Jamie Jamahl de la Sencerie El-Dessouky <jael2@elev.tec.dk>
2026-06-19 15:54:16 +0200

    adds beginning to Javascript part of foundation course

---
+++ javascript/javascript.js
@@ -0,0 +1,18 @@
+let firstname = "john";
+let lastname = "doe";
+let age = 30;
+
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+
+age = 35;
+
+console.log(age);
+
+const pi = 3.14;
+
+// pi = 10; - CONST's kan ikke overrides. Så den vil fejle her.
+
+console.log(pi);
+
