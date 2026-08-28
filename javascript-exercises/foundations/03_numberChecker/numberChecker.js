ecfb13f12b0676cec974b0d582e37b78bd498aae
Jamie Jamahl de la Sencerie El-Dessouky <jael2@elev.tec.dk>
2026-08-28 12:03:00 +0200

    Convert javascript-exercises from submodule to regular folder

---
+++ javascript-exercises/foundations/03_numberChecker/numberChecker.js
@@ -0,0 +1,10 @@
+function numberChecker(number) {
+  if (number >= 10) { // havde lavet en fejl og sat den til 6 her.
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
