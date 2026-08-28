9b9042d8574df31eb66b0549c5bffa4243e9472d
Jamie Jamahl de la Sencerie El-Dessouky <jael2@elev.tec.dk>
2026-08-28 14:50:27 +0200

    added number guessing

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -122,3 +122,48 @@ console.log(typeof myNums);
 const myNums2 = "666";
 const myString2 = String(myNums2);
 console.log(typeof myString2);
+
+function add7(num) {
+    return num + 7;
+}
+
+console.log(add7(10));
+
+function multiply(numb1, numb2) {
+    return numb1 * numb2;
+} 
+
+console.log(multiply(3, 2));
+
+function capitalize(letters) {
+    return letters.charAt(0).toUpperCase() + letters.slice(1).toLowerCase();
+}
+
+console.log(capitalize("abcd"));
+console.log(capitalize("ABCD"));
+console.log(capitalize("aBcD"));
+
+function lastLetter(lastLet) {
+    return lastLet.slice(3);
+}
+
+console.log(lastLetter("abcd"));
+
+// Fizz Buzz
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+// tæl op til det ønskede nummer. i er nummeret vi starter på. i <= answer; er det mål vi gerne vil nå, dvs. vi vil gerne loope indtil i er større end answer. i++ siger at vores loop skal gå op for hver iteration. 
+for (let i = 1; i <= answer; i++) { // let i = 1; betydeer at i er 1. 
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
+
+//If you haven’t seen this before and it looks strange, it’s actually straightforward. We declare a variable i and assign it 1: the initial value of the variable i in our loop. The second clause, i <= answer is our condition. We want to loop until i is greater than answer. The third clause, i++, tells our loop to increment i by 1 every iteration. As a result, if the user inputs 10, this loop would print numbers 1 - 10 to the console.
